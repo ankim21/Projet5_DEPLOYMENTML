d8de5d7c516af5bc50179ff918a22cc5c43fed8b
ankim <ankim@f1br4s3.paris.42.school>
2026-09-03 17:28:41 +0200

    [feat/HF] : understanding FastAPI + Pydantic

--- app/main.py
+++ app/main.py
@@ -6,11 +6,50 @@ app = FastAPI(title="Futurisys API")
 # async def root():
 #     return {"message": "Hello World"}
 
-@app.get("/")
-def root():
-    return {"status": "ok"}
+# @app.get("/")
+# def root():
+#     return {"status": "ok"}
 
 
-@app.get("/health")
-def health():
-    return {"status": "healthy"}
+# @app.get("/health")
+# def health():
+#     return {"status": "healthy"}
+
+@app.get("/hello")
+def hello(name: str):
+    return {"message": f"Hello {name}"}
+
+# so if app is deployed, and another service calls:
+# GET /hello?name=Alice
+# we get : "message": "Hello Alice"
+
+
+from pydantic import BaseModel
+
+class User(BaseModel):
+    name: str
+    age: int
+    email: str
+
+user = User(
+    name="Alice",
+    age=32,
+    email="alice@example.com"
+)
+
+## if we mix them:
+
+class UserType(BaseModel):
+    name: str
+    age: int
+
+
+@app.post("/users")
+def create_user(user: UserType):
+    return {
+        "message": f"Created {user.name}",
+        "age": user.age
+    }
+
+#FAST API receives HTTP request, FastAPI gives function a proper User object
+
